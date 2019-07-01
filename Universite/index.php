1078e56b8a66685d956bfdd57cbe378d0fd89fc3
makam1 <46441644+makam1@users.noreply.github.com>
2019-07-01 08:54:32 +0000

    Add files via upload

---
+++ Universite/index.php
@@ -0,0 +1,18 @@
+<?php
+    include "header.php";
+    
+?>
+<section>
+ <div class="slider">
+            <div class="content">
+                <div class="principal">
+                    <h1>SA UNIVERSITE</h1>
+                </div>
+            </div>
+        </section>
+
+
+      <?php
+    include "footer.php";
+   
+?>
